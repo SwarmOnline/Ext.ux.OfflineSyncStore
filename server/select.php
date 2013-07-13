976ff4c11b0e1c43b5681cc08577e5a60ed65630
Andrew Duncan <andrew@swarmonline.com>
2013-07-13 15:09:31 +0100

    Updated example to work with Sencha Touch 2.2.1

--- server/select.php
+++ server/select.php
@@ -5,13 +5,13 @@
         'total' => 2,
         'rows' => array(
             array(
-                'PersonID' => 1,
+                'PersonID' => "10f54ef9-6c9d-4974-ae5d-70b463bfefff",
                 'FirstName' => 'Stuart',
                 'LastName' => 'Ashworth',
                 'Email' => 'stuart@swarmonline.com'
             ),
             array(
-                'PersonID' => 2,
+                'PersonID' => "3b3aae5d-4108-4a45-bc35-d31b7874eb6b",
                 'FirstName' => 'Andrew',
                 'LastName' => 'Duncan',
                 'Email' => 'andrew@swarmonline.com'
